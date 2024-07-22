fc6bd8cda8ce1145bd6e0626eb22d7223f033073
Elena <56148816+elenajusto@users.noreply.github.com>
2024-07-22 16:42:55 +1000

    modified Python script to only produce altitude data stream in text file

--- prototype2.1/readWriteTest/readWriteTest.ino
+++ prototype2.1/readWriteTest/readWriteTest.ino
@@ -1,21 +1,6 @@
 /*
-  SD card read/write
-
-  This example shows how to read and write data to and from an SD card file
-  The circuit:
-   SD card attached to SPI bus as follows:
- ** MOSI - pin 11
- ** MISO - pin 12
- ** CLK - pin 13
- ** CS - pin 4 (for MKRZero SD: SDCARD_SS_PIN)
-
-  created   Nov 2010
-  by David A. Mellis
-  modified 9 Apr 2012
-  by Tom Igoe
-
-  This example code is in the public domain.
-
+  Modified SD card read/write to return the values stored in a text based
+  csv file containing pressure data.
 */
 
 /* Includes */
@@ -25,11 +10,19 @@
 /* Variables */
 File myFile;
 
+int numberOfTime;             // Store number of time measurements from csv
+int numberOfAltitude;         // Store number of altitude measurements from csv
+int numberOfPressure;         // Store number of pressure measurements from csv
+
 /* Function Prototypes */
 void readFromCard();
+void getTimeNumber();
+void getAltitudeNumber();
+void getPressureNumber();
 
 /* Main Program */
 void setup() {
+  
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
@@ -65,10 +58,27 @@ void readFromCard()
     while (myFile.available()) {
       Serial.write(myFile.read());
     }
+    
     // close the file:
     myFile.close();
+    
   } else {
     // if the file didn't open, print an error:
     Serial.println("error opening test.txt");
   }
+}
+
+void getTimeNumber()
+{
+  
+}
+
+void getAltitudeNumber()
+{
+  
+}
+
+void getPressureNumber()
+{
+  
 }
